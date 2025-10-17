f789e63da4c1e00df7dfde7608f733c555952651
CesValde <valderramacesar2002@gmail.com>
2025-10-16 21:59:07 -0300

    funcion para filtrar las ofertas
    reemplazamos el evento por una funcion, asi la usamos desde el fetch-data

--- Clase2/resultados_busqueda/filters.js
+++ Clase2/resultados_busqueda/filters.js
@@ -1,35 +1,30 @@
 // cada vez que se aplique un filtro se escuchara el evento que mostrara las ofertas con dicho filtro 
-const filtros = document.querySelector('.container-filtros')
+export function filtrarOfertas(allJobs) {
+    const filtros = document.querySelector('.container-filtros')
+    const input = document.querySelector('input')
 
-filtros.addEventListener('change', () => {
-    // obtengo todos los select
-    const jobs = document.querySelectorAll('.ofert-jobs')
-    // array.from convierte ese NodeList en un array real de JavaScript (para poder usar .map() y otros métodos de array).
-    // .map(s => s.value) recorre cada <select> y devuelve su valor actual seleccionado (s.value), 'toma el value de cada select'
-    const [tecnologia, modalidad, contrato, nivel] = Array.from(filtros.querySelectorAll('select')).map(s => s.value)
+    const texto = input.value.toLowerCase().trim()
+    const [tecnologia, modalidad, contrato, nivel] = Array.from(
+        filtros.querySelectorAll('select')
+    ).map(s => s.value)
 
-    // se recorren cada oferta y muestra las ofertas en base a los filtros seleccionados
-    jobs.forEach(job => {
+    // filtrar a nivel de datos, no de elementos DOM, recorre cada oferta y retorna las ofertas en base a los filtros seleccionados
+    return allJobs.filter(job => {
+        const techs = Array.isArray(job.data.technology)
+        ? job.data.technology
+        : [job.data.technology]
 
-        let techs = []
-        // hago try catch porque si es un string lo toma como error al hacer '.parse'
-        try {
-            techs = JSON.parse(job.dataset.technology)  // intento parsear
-            if (!Array.isArray(techs)) techs = [techs]  // si no es array, lo convierto en array
-        } catch (e) {
-            techs = [job.dataset.technology]  // si falla, guardo el valor como array con un solo elemento
-        }
-
+        // validacion de selects
         const coincideTecnologia = tecnologia === '' || techs.includes(tecnologia)
-        const coincideModalidad = modalidad === '' || job.dataset.modalidad === modalidad
-        const coincideContrato  = contrato === '' || job.dataset.contrato === contrato
-        const coincideNivel = nivel === '' || job.dataset.nivel === nivel
+        const coincideModalidad = modalidad === '' || job.data.modalidad === modalidad
+        const coincideContrato = contrato === '' || job.data.contrato === contrato
+        const coincideNivel = nivel === '' || job.data.nivel === nivel
 
-        // si todos coinciden → mostrar
-        if (coincideTecnologia && coincideModalidad && coincideContrato && coincideNivel) {
-            job.style.display = 'block'
-        } else {
-            job.style.display = 'none'
-        }
+        // validacion de input
+        const titulo  = job.titulo.toLowerCase()
+        const empresa = job.empresa.toLowerCase()
+        const coincideBusqueda = texto === '' || titulo.includes(texto) || empresa.includes(texto)
+
+        return coincideTecnologia && coincideModalidad && coincideContrato && coincideNivel && coincideBusqueda
     })
-})
+}
