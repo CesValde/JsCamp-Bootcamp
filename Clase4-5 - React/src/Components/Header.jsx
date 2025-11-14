274a119e57356641e464eec0b8634b0ab52d76ad
CesValde <valderramacesar2002@gmail.com>
2025-11-14 20:30:08 -0300

    img, json, style
    agrego la imagen en home, el json es array, y estilo en linea de un h2

--- Clase4-5 - React/src/Components/Header.jsx
+++ Clase4-5 - React/src/Components/Header.jsx
@@ -3,8 +3,8 @@ import '../js/devjobs-avatar-element.js'
 
 export function Header() {
    return (
-         <header className={styles.header}> 
-            <div className={styles.headLeft}>
+         <header className={styles.headerStyle}> 
+            <div className={styles.leftSide}>
                <svg width="34" height="34" fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path clipRule="evenodd" d="M12.08 24 4 19.248 9.955 8.752l8.085 4.744L18.045 4h11.91l.005 9.496 8.085-4.744L44 19.248 35.92 24 44 28.752l-5.955 10.496-8.085-4.744L29.955 44h-11.91l-.005-9.496-8.085 4.744L4 28.752z" fill="currentColor" fillRule="evenodd"/></svg>
                <a href=""> DevJobs </a>
                <a href=""> Inicio </a>
@@ -12,9 +12,9 @@ export function Header() {
                <a href=""> Empresas </a>
                <a href=""> Salarios </a>
             </div>
-            <div className={styles.headRight}> 
-               <button className={styles.btn}> Subir CV </button>
-               <devjobs-avatar username="CesValde" size="36" className={styles.avatar}> </devjobs-avatar>
+            <div className={styles.rightSide}> 
+               <button> Subir CV </button>
+               <devjobs-avatar username="CesValde" size="36"> </devjobs-avatar>
             </div>
          </header>
    )
--- Clase4-5 - React/src/Components/OfertJobs.jsx
+++ Clase4-5 - React/src/Components/OfertJobs.jsx
@@ -4,6 +4,8 @@ import styles from '../OfertJobs.module.css'
 export function OfertJobs({ jobs }) {
    return (
       <>
+         <h2 style={{ textAlign: "center", padding: "20px 0" }}> Resultados de Busqueda </h2>
+
          <div className={styles.ofertJobs}> 
             {jobs.map(job => (
                <JobCard key={job.id} job={job} /> 
