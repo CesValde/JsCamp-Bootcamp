8b49b6ef3fab18cb90170cc4fa8d3249b445da0f
CesValde <valderramacesar2002@gmail.com>
2026-01-24 19:18:02 -0300

    API
    change: deploy api in vercel, use in frontend to call jobs

--- Clase6 -7 - rRouter rContext Zustand/src/pages/Search.jsx
+++ Clase6 -7 - rRouter rContext Zustand/src/pages/Search.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react'
-import { useSearchParams } from 'react-router'
-import { SearchFilters } from '../Components/SearchFilters.jsx'
-import { OfertJobs } from '../Components/OfertJobs.jsx'
-import { Pagination } from '../Components/Pagination.jsx'
-import { useRouter } from '../hooks/useRouter.jsx'
+import { useState, useEffect } from "react"
+import { useSearchParams } from "react-router"
+import { SearchFilters } from "../Components/SearchFilters.jsx"
+import { OfertJobs } from "../Components/OfertJobs.jsx"
+import { Pagination } from "../Components/Pagination.jsx"
+import { useRouter } from "../hooks/useRouter.jsx"
 
 const RESULTS_PER_PAGE = 4
 
@@ -11,19 +11,20 @@ const useFilters = () => {
    const [searchParams, setSearchParams] = useSearchParams()
 
    const [filters, setFilters] = useState(() => {
-
       return {
-         technology: searchParams.get('technology') || '',
-         location: searchParams.get('type') || '',
-         experienceLevel: searchParams.get('level') || ''
+         technology: searchParams.get("technology") || "",
+         location: searchParams.get("type") || "",
+         experienceLevel: searchParams.get("level") || ""
       }
    })
 
-   const [textToFilter, setTextToFilter] = useState(() => searchParams.get('text') || '')
+   const [textToFilter, setTextToFilter] = useState(
+      () => searchParams.get("text") || ""
+   )
 
    const [currentPage, setCurrentPage] = useState(() => {
-      const page = Number(searchParams.get('page'))
-      return Number.isNaN(page) ? page : 1 
+      const page = Number(searchParams.get("page"))
+      return Number.isNaN(page) ? page : 1
    })
 
    const [jobs, setJobs] = useState([])
@@ -38,23 +39,28 @@ const useFilters = () => {
             setLoading(true)
 
             const params = new URLSearchParams()
-            if(textToFilter) params.append('text', textToFilter)
-            if(filters.technology) params.append('technology', filters.technology)
-            if(filters.location) params.append('type', filters.location)
-            if(filters.experienceLevel) params.append('level', filters.experienceLevel)
+            if (textToFilter) params.append("text", textToFilter)
+            if (filters.technology)
+               params.append("technology", filters.technology)
+            if (filters.location) params.append("type", filters.location)
+            if (filters.experienceLevel)
+               params.append("level", filters.experienceLevel)
 
-            const offset = (currentPage -1 ) * RESULTS_PER_PAGE
-            params.append('limit', RESULTS_PER_PAGE)
-            params.append('offset', offset)
+            const offset = (currentPage - 1) * RESULTS_PER_PAGE
+            params.append("limit", RESULTS_PER_PAGE)
+            params.append("offset", offset)
 
             const queryParams = params.toString()
-            const response = await fetch(`https://jscamp-api.vercel.app/api/jobs?${queryParams}`)
+
+            const response = await fetch(
+               `https://clase8-express-4pssa3ish-cesar-valderramas-projects.vercel.app/jobs?${queryParams}`
+            )
             const json = await response.json()
 
             setJobs(json.data)
             setTotal(json.total)
          } catch (error) {
-            console.error('Error fetching jobs: ', error)
+            console.error("Error fetching jobs: ", error)
          } finally {
             setLoading(false)
          }
@@ -63,20 +69,21 @@ const useFilters = () => {
       fetchJobs()
    }, [filters, textToFilter, currentPage])
 
-   useEffect(() => { 
-       setSearchParams((params) => {
-         if(textToFilter) params.set('text', textToFilter)
-         if(filters.technology) params.set('technology', filters.technology)
-         if(filters.location) params.set('type', filters.location)
-         if(filters.experienceLevel) params.set('level', filters.experienceLevel)
+   useEffect(() => {
+      setSearchParams((params) => {
+         if (textToFilter) params.set("text", textToFilter)
+         if (filters.technology) params.set("technology", filters.technology)
+         if (filters.location) params.set("type", filters.location)
+         if (filters.experienceLevel)
+            params.set("level", filters.experienceLevel)
 
-         if(currentPage > 1) params.set('page', currentPage)
+         if (currentPage > 1) params.set("page", currentPage)
 
          return params
       })
    }, [filters, textToFilter, currentPage, setSearchParams])
 
-   // renderiza luego de filtrar 
+   // renderiza luego de filtrar
    const totalPages = Math.ceil(total / RESULTS_PER_PAGE)
 
    // actualiza el estado de la pagina actual
@@ -107,12 +114,12 @@ const useFilters = () => {
    }
 }
 
-export default function SearchPage () {
-   const  {
+export default function SearchPage() {
+   const {
       jobs,
       total,
       loading,
-      totalPages, 
+      totalPages,
       currentPage,
       textToFilter,
       handlePageChange,
@@ -120,24 +127,31 @@ export default function SearchPage () {
       handleTextFilter
    } = useFilters()
 
-   const title = loading 
-   ? `Cargando... - DebJobs` 
-   : `Resultados: ${total}, Pagina ${currentPage} - DevJobs`
+   const title = loading
+      ? `Cargando... - DebJobs`
+      : `Resultados: ${total}, Pagina ${currentPage} - DevJobs`
 
    return (
       <>
          <title> {title} </title>
-         <main> 
-            <SearchFilters 
-               initialText={textToFilter} 
-               onSearch={handleSearch} 
-               onTextFilter={handleTextFilter}  
+         <main>
+            <SearchFilters
+               initialText={textToFilter}
+               onSearch={handleSearch}
+               onTextFilter={handleTextFilter}
             />
-        
-            <section> 
-               <h2 style={{ textAlign: "center", padding: "20px 0" }}> Resultados de Busqueda </h2>
-               <OfertJobs jobs={jobs}/>
-               <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+
+            <section>
+               <h2 style={{ textAlign: "center", padding: "20px 0" }}>
+                  {" "}
+                  Resultados de Busqueda{" "}
+               </h2>
+               <OfertJobs jobs={jobs} />
+               <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+               />
             </section>
          </main>
       </>
