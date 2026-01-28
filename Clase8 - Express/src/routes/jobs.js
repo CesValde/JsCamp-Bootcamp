5fef679706361ec7b7c2dc6bb59d7a5f0ece54b8
CesValde <valderramacesar2002@gmail.com>
2026-01-27 21:45:17 -0300

    add: middlewares
    validate Create and update jobs

--- Clase8 - Express/src/routes/jobs.js
+++ Clase8 - Express/src/routes/jobs.js
@@ -1,11 +1,34 @@
 import { Router } from "express"
 import { JobController } from "../controllers/jobs.js"
+import { validateJob, validatePartialJob } from "../schemas/jobs.js"
 
 export const jobsRouter = Router()
 
+function validateCreate(req, res, next) {
+   const result = validateJob(req.body)
+   if (result.success) {
+      req.body = result.data
+      return next()
+   }
+
+   return res
+      .status(400)
+      .json({ error: "Invalidad request", details: result.error.errors })
+}
+
+function validateUpdate(req, res, next) {
+   const result = validatePartialJob(req.body)
+   if (result.success) {
+      req.body = result.data
+      return next()
+   }
+
+   return res.status(400).json({ error: JSON.parse(result.error.message) })
+}
+
 jobsRouter.get("/", JobController.getAll)
 jobsRouter.get("/:id", JobController.getId)
-jobsRouter.post("/", JobController.create)
-jobsRouter.put("/:id", JobController.update)
+jobsRouter.post("/", validateCreate, JobController.create)
+jobsRouter.put("/:id", validateUpdate, JobController.update)
 jobsRouter.patch("/:id", JobController.partialUpdate)
 jobsRouter.delete("/:id", JobController.delete)
