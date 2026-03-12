ba74e8547a8d31cd4b1c742768f321182e1b5fe2
CesValde <valderramacesar2002@gmail.com>
2026-03-12 16:24:31 -0300

    try fix vercel deploy

--- 08-Express/api/jobs.js
+++ 08-Express/api/jobs.js
@@ -2,7 +2,7 @@ import fs from "fs"
 
 export default function handler(req, res) {
    try {
-      const filePath = new URL("../data/jobs.json", import.meta.url)
+      const filePath = new URL("../src/data/jobs.json", import.meta.url)
       const jobs = JSON.parse(fs.readFileSync(filePath, "utf-8"))
 
       res.status(200).json(jobs)
