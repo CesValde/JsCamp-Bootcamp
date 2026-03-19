a6ae0a04bc4f420b8020d1d5cce009aa49be8bda
CesValde <valderramacesar2002@gmail.com>
2026-03-19 15:56:19 -0300

    add: aiRouter, proxy

--- 11 - IA/Backend/src/app.js
+++ 11 - IA/Backend/src/app.js
@@ -1,24 +1,26 @@
+process.loadEnvFile()
 import express from "express"
 import { jobsRouter } from "./routes/jobs.js"
 import { corsMiddleware } from "./middleware/cors.js"
 import { DEFAULTS } from "./config.js"
+import { aiRouter } from "./routes/ai.js"
 
 const PORT = process.env.PORT ?? DEFAULTS.PORT
 const app = express()
+
+// toma la verdadera IP del cliente cuando se está detrás de un proxy (como en producción con servicios como Heroku o Vercel)
+app.set("trust proxy", 1)
 
 app.use(corsMiddleware())
 app.use(express.json())
 
 app.use("/jobs", jobsRouter)
+app.use("/ai", aiRouter)
 
-if (!process.env.NODE_ENV) {
+if (process.env.NODE_ENV !== "production") {
    app.listen(PORT, () => {
-      console.log(process.env.NODE_ENV)
       console.log(`Servidor levantado en http://localhost:${PORT}`)
    })
 }
-console.log(process.env.NODE_ENV)
 
 export default app
-
-// npm run dev:prod
